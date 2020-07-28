618050f9cedafd1367703dc9b8b14abec5fb1689
Yucel Cekinmez <cekinmezyucel@gmail.com>
2020-07-28 22:25:33 +0200

    pre-commit fine tune

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_network" "network" {
-  name                                 = var.name
+  name                    = var.name
   project                 = var.project_id
   auto_create_subnetworks = var.auto_create_subnetworks
   routing_mode            = var.routing_mode
